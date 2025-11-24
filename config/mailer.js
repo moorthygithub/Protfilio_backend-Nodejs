a8c3c93f2163387b9733345875e41c881ae9e83c
Moorthy <moorthy.chandiran21@gmail.com>
2025-11-24 23:06:57 +0530

    proftolio post issue fixed

--- config/mailer.js
+++ config/mailer.js
@@ -1,11 +1,32 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// module.exports = transporter;
+// transporter.js
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587, // TLS port
+  secure: false, // use TLS
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER, // Gmail address
+    pass: process.env.EMAIL_PASS, // App password
   },
+  tls: {
+    rejectUnauthorized: false, // important for some cloud servers
+  },
+  connectionTimeout: 20000, // 20 seconds
+  greetingTimeout: 20000,
+  socketTimeout: 20000,
+  debug: true, // logs SMTP activity
 });
 
 module.exports = transporter;
--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// const isProduction = process.env.NODE_ENV == "production";
-
-// require("dotenv").config({
-//   path: isProduction ? "/etc/secrets/env" : ".env",
-// });
-// // Routes
-// app.use("/uploads", express.static("uploads"));
-
-// const profolioRoutes = require("./routes/profolioRoutes");
-// const loginRoutes = require("./routes/loginRoutes");
-// const sinupRoutes = require("./routes/SignRoute");
-// const productRoutes = require("./routes/productRoute");
-// const verifyToken = require("./middleware/auth");
-// const upload = require("./middleware/upload");
-// app.use("/api/profolio", loginRoutes);
-// app.use("/api/profolio", sinupRoutes);
-
-// app.use("/api/profolio", verifyToken, profolioRoutes);
-// app.use("/api/product", verifyToken, upload.array("Images", 5), productRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// const ENV = process.env.DATABASE_URL;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server is running on port ${PORT}`);
-//   console.log(`🚀 env ${ENV}`);
-// });
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -59,10 +26,10 @@ app.get("/", (req, res) => {
 });
 app.use("/api/profolio", loginRoutes);
 app.use("/api/profolio", sinupRoutes);
-app.use("/api/profolio",  profolioRoutes);
+// app.use("/api/profolio", verifyToken, profolioRoutes);
+app.use("/api/profolio", profolioRoutes);
 app.use("/api/product", verifyToken, upload.array("Images", 5), productRoutes);
 
-// Test base route
 app.get("/", (req, res) => {
   res.send("🚀 API is running...");
 });
